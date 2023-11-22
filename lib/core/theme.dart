40d99972191cc03c34fd4bb1caeb5908d718b157
AnneAlmd <annealmd@gmail.com>
2023-11-22 13:23:24 -0300

    tela login

---
+++ lib/core/theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color colorOne = const Color(0xFF1f5567);
+Color colorTwo = const Color(0xFF2d958e);
+Color colorButton = const Color(0xFF44bd6e);
---
+++ lib/views/login_page.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:target_test/core/theme.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController userController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    userController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: colorTwo,
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [colorOne, colorTwo],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 100),
+            const Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                'Usuário',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            TextFormField(
+              controller: userController,
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
+              onTapOutside: (event) => FocusScope.of(context).unfocus(),
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.person),
+                //labelText: 'Usuário',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Informe um email';
+                } else if (!value.contains('@')) {
+                  return 'Informe um email válido';
+                } else {
+                  return null;
+                }
+              },
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                'Senha',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            TextFormField(
+              controller: passwordController,
+              obscureText: true,
+              textInputAction: TextInputAction.next,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.lock),
+                labelText: 'Senha',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              validator: (value) {
+                if (value!.length < 6) {
+                  return 'Deve ter no mínimo 6 caracteres';
+                } else if (!RegExp(r'^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]+$')
+                    .hasMatch(value)) {
+                  return 'Deve conter pelo menos uma letra maiúscula e um número';
+                } else {
+                  return null;
+                }
+              },
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: colorButton,
+                  foregroundColor: Colors.white,
+                  elevation: 8,
+                  minimumSize: const Size(200, 40),
+                ),
+                child: const Text('Entrar'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+          color: colorTwo,
+          width: double.infinity,
+          alignment: Alignment.center,
+          height: 80,
+          child: TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Política de Privacidade',
+                style: TextStyle(color: Colors.white),
+              ))),
+    );
+  }
+}
