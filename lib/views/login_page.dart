744fb4ee4e0e683eff8fcc5424fe3b7b26089de0
AnneAlmd <annealmd@gmail.com>
2023-11-22 14:27:52 -0300

    fix form
    overflow and clear field when not validated

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:target_test/core/theme.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,14 +10,22 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
   TextEditingController userController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  final Uri _url = Uri.parse('https://google.com.br');
 
   @override
   void dispose() {
     userController.dispose();
     passwordController.dispose();
     super.dispose();
+  }
+
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
   }
 
   @override
@@ -34,91 +43,105 @@ class _LoginPageState extends State<LoginPage> {
             colors: [colorOne, colorTwo],
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 100),
-            const Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: Text(
-                'Usuário',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            TextFormField(
-              controller: userController,
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.next,
-              onTapOutside: (event) => FocusScope.of(context).unfocus(),
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.person),
-                //labelText: 'Usuário',
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
+        child: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 100),
+                const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                    'Usuário',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Informe um nome de usuário';
-                } else if (value.length > 21) {
-                  return 'Deve ter no máximo 20 caracteres';
-                } else if (!RegExp(r'^[a-zA-Z0-9\s]*[^\s]$').hasMatch(value)) {
-                  return 'DNão deve conter espaço em branco no final.';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: Text(
-                'Senha',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            TextFormField(
-              controller: passwordController,
-              obscureText: true,
-              textInputAction: TextInputAction.next,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.lock),
-                labelText: 'Senha',
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
+                TextFormField(
+                  controller: userController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.person),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Informe um nome de usuário';
+                    } else if (value.length > 21) {
+                      userController.clear();
+                      return 'Deve ter no máximo 20 caracteres';
+                    } else if (!RegExp(r'^[a-zA-Z0-9\s]*[^\s]$')
+                        .hasMatch(value)) {
+                          userController.clear();
+                      return 'Não deve conter espaço em branco no final.';
+                    } else {
+                      return null;
+                    }
+                  },
                 ),
-              ),
-              validator: (value) {
-                if (value!.length < 2) {
-                  return 'Deve ter no mínimo 2 caracteres';
-                } else if (value.length > 21) {
-                  return 'Deve ter no máximo 20 caracteres';
-                } else if (!RegExp(r'^[a-zA-Z0-9]*[^\\s]$').hasMatch(value)) {
-                  return 'Deve conter apenas letras e números';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: colorButton,
-                  foregroundColor: Colors.white,
-                  elevation: 8,
-                  minimumSize: const Size(200, 40),
+                const SizedBox(height: 20),
+                const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                    'Senha',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
-                child: const Text('Entrar'),
-              ),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.lock),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.length < 2) {
+                      passwordController.clear();
+                      return 'Deve ter no mínimo 2 caracteres';
+                    } else if (value.length > 21) {
+                      passwordController.clear();
+                      return 'Deve ter no máximo 20 caracteres';
+                    } else if (!RegExp(r'^[a-zA-Z0-9]*[^\\s]$')
+                        .hasMatch(value)) {
+                          passwordController.clear();
+                      return 'Deve conter apenas letras e números';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        debugPrint('Everything alright!');
+                      } else {}
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: colorButton,
+                      foregroundColor: Colors.white,
+                      elevation: 8,
+                      minimumSize: const Size(200, 40),
+                    ),
+                    child: const Text('Entrar'),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
       bottomNavigationBar: Container(
@@ -127,7 +150,7 @@ class _LoginPageState extends State<LoginPage> {
           alignment: Alignment.center,
           height: 80,
           child: TextButton(
-              onPressed: () {},
+              onPressed: _launchUrl,
               child: const Text(
                 'Política de Privacidade',
                 style: TextStyle(color: Colors.white),
