7dc4fa3c1b4c3c125cb00ef0a362f5705cae0620
AnneAlmd <annealmd@gmail.com>
2023-11-23 15:46:29 -0300

    snackbar
    campo vazio na info_page

--- lib/views/functions/functions.dart
+++ lib/views/functions/functions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:target_test/stores/info_store.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> _launchUrl() async {
@@ -11,8 +12,6 @@ Future<void> _launchUrl() async {
 Future<void> callGoogle() async {
   await _launchUrl();
 }
-
-
 
 Future<void> myDialogBuilder({
   required BuildContext context,
@@ -46,6 +45,60 @@ Future<void> myDialogBuilder({
             child: const Text('Apagar'),
             onPressed: () {
               buttonAction;
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+myShowSnackBar(
+    {required BuildContext context,
+    required String message,
+    bool isError = true}) {
+  var snackBar = SnackBar(
+    elevation: 8,
+    backgroundColor: isError
+        ? const Color.fromARGB(255, 153, 12, 2)
+        : const Color.fromARGB(255, 1, 90, 49),
+    content: Text(message),
+    duration: const Duration(seconds: 3),
+    showCloseIcon: true,
+  );
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+Future<void> myShowDialog(BuildContext context, int i, InfoStore infoStore) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Apagar Informação'),
+        content: const Text(
+          'A informação será apagada e não será possível recuperá-la.',
+        ),
+        actions: <Widget>[
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: Theme.of(context).textTheme.labelLarge,
+            ),
+            child: const Text('Cancelar'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: Theme.of(context).textTheme.labelLarge,
+            ),
+            child: const Text(
+              'Apagar',
+              style: TextStyle(color: Color.fromARGB(255, 134, 10, 1)),
+            ),
+            onPressed: () {
+              infoStore.deleteInfo(i);
               Navigator.of(context).pop();
             },
           ),
--- lib/views/info_page.dart
+++ lib/views/info_page.dart
@@ -97,7 +97,7 @@ class _InfoPageState extends State<InfoPage> {
                                             Color.fromARGB(255, 187, 16, 4)),
                                   ),
                                   onPressed: () {
-                                    myShowDialog(context, i);
+                                    myShowDialog(context, i, _infoStore);
 
                                     //
                                   },
@@ -132,7 +132,8 @@ class _InfoPageState extends State<InfoPage> {
                 ),
                 onSubmitted: (value) {
                   if (infoTextController.text.isEmpty) {
-                    //TODO popup
+                    myShowSnackBar(
+                        context: context, message: 'Digite o seu Texto');
                   } else {
                     editText
                         ? _infoStore.editInfo(infoToBeEdited['index'],
@@ -155,49 +156,5 @@ class _InfoPageState extends State<InfoPage> {
         child: const PoliticaPrivacidadeButton(),
       ),
     );
-  }
-
-  Future<void> myShowDialog(BuildContext context, int i) {
-    return showDialog<void>(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title:
-                                            const Text('Apagar Informação'),
-                                        content: const Text(
-                                          'A informação será apagada e não será possível recuperá-la.',
-                                        ),
-                                        actions: <Widget>[
-                                          TextButton(
-                                            style: TextButton.styleFrom(
-                                              textStyle: Theme.of(context)
-                                                  .textTheme
-                                                  .labelLarge,
-                                            ),
-                                            child: const Text('Cancelar'),
-                                            onPressed: () {
-                                              Navigator.of(context).pop();
-                                            },
-                                          ),
-                                          TextButton(
-                                            style: TextButton.styleFrom(
-                                              textStyle: Theme.of(context)
-                                                  .textTheme
-                                                  .labelLarge,
-                                            ),
-                                            child: const Text(
-                                              'Apagar',
-                                              style: TextStyle(
-                                                  color: Color.fromARGB(255, 134, 10, 1)),
-                                            ),
-                                            onPressed: () {
-                                              _infoStore.deleteInfo(i);
-                                              Navigator.of(context).pop();
-                                            },
-                                          ),
-                                        ],
-                                      );
-                                    },
-                                  );
   }
 }
